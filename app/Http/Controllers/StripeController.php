a9c4ef56c6526bc8aefdfdbe4572181aa36a9532
Saquibul Hassan Razib <saquibulhassan@gmail.com>
2020-12-24 03:08:57 +0600

    Authorization hold, capture & release added

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Stripe\Balance;
+use Stripe\Charge;
+use Stripe\Collection;
+use Stripe\Customer;
+use Stripe\Exception\ApiErrorException;
 use Stripe\StripeClient;
 
 class StripeController extends Controller
@@ -18,17 +22,79 @@ class StripeController extends Controller
     /**
      * Create a customer at stripe
      *
-     * @return \Stripe\Customer
-     * @throws \Stripe\Exception\ApiErrorException
+     * @return Collection
+     * @throws ApiErrorException
+     */
+    public function customers()
+    {
+        $customer = $this->stripe->customers->all();
+        return $customer;
+    }
+
+    /**
+     * Create a customer at stripe
+     *
+     * @return Customer
+     * @throws ApiErrorException
      */
     public function customerCreate()
     {
         $customer = $this->stripe->customers->create(
             [
-                'description'    => 'Saquibul Hasan Razib',
-                'email'          => 'saquib@test.com',
+                'description'    => 'Amir Hossain',
+                'email'          => 'amir@test.com',
                 'payment_method' => 'pm_card_visa',
+                'balance'        => -5000000,
             ]
+        );
+        return $customer;
+    }
+
+    /**
+     * Create a customer at stripe
+     *
+     * @return Customer
+     * @throws ApiErrorException
+     */
+    public function retriveCustomer(Request $request)
+    {
+        $customer = $this->stripe->customers->retrieve($request->cutomer_id, []);
+        return $customer;
+    }
+
+    /**
+     * Update a customer at stripe
+     *
+     * @return Customer
+     * @throws ApiErrorException
+     */
+    public function updateCustomer(Request $request)
+    {
+        $customer = $this->stripe->customers->update(
+            $request->cutomer_id,
+            [
+                'description' => 'Mehedi Hasan',
+                'email'       => 'mehedi@test.com',
+                'balance'     => -5000000,
+                'metadata'    => [
+                    'order_id' => '6735',
+                ],
+            ]
+        );
+        return $customer;
+    }
+
+    /**
+     * Delete a customer at stripe
+     *
+     * @return Customer
+     * @throws ApiErrorException
+     */
+    public function deleteCustomer(Request $request)
+    {
+        $customer = $this->stripe->customers->delete(
+            $request->cutomer_id,
+            []
         );
         return $customer;
     }
@@ -36,18 +102,18 @@ class StripeController extends Controller
     /**
      * Create a charge from customer
      *
-     * @return \Stripe\Charge
-     * @throws \Stripe\Exception\ApiErrorException
+     * @return Charge
+     * @throws ApiErrorException
      */
     public function createCharge()
     {
         $charge = $this->stripe->charges->create(
             [
-                 'amount' => 2000,
-                 'currency' => 'usd',
-                 'source' => 'tok_mastercard',
-                 'description' => 'My First Test Charge (created for API docs)',
-             ]
+                'amount'      => 2000,
+                'currency'    => 'usd',
+                'source'      => 'tok_mastercard',
+                'description' => 'My First Test Charge (created for API docs)',
+            ]
         );
         return $charge;
     }
@@ -55,19 +121,19 @@ class StripeController extends Controller
     /**
      * Create authorize hold for customer
      *
-     * @return \Stripe\Charge
-     * @throws \Stripe\Exception\ApiErrorException
+     * @return Charge
+     * @throws ApiErrorException
      */
     public function authorizeHold()
     {
         $charge = $this->stripe->charges->create(
             [
-                 'amount' => 20000,
-                 'currency' => 'usd',
-                 'source' => 'tok_mastercard',
-                 'description' => 'My First Authorize Hold of 200 USD',
-                 'capture' => false,
-             ]
+                'amount'      => 20000,
+                'currency'    => 'usd',
+                'source'      => 'tok_mastercard',
+                'description' => 'My First Authorize Hold of 200 USD',
+                'capture'     => false,
+            ]
         );
         return $charge;
     }
@@ -75,8 +141,8 @@ class StripeController extends Controller
     /**
      * Create a charge from customer
      *
-     * @return \Stripe\Charge
-     * @throws \Stripe\Exception\ApiErrorException
+     * @return Charge
+     * @throws ApiErrorException
      */
     public function authorizeHoldCapture(Request $request)
     {
@@ -89,8 +155,8 @@ class StripeController extends Controller
     /**
      * Retrieve charge by transaction id
      *
-     * @return \Stripe\Charge
-     * @throws \Stripe\Exception\ApiErrorException
+     * @return Charge
+     * @throws ApiErrorException
      */
     public function retrieveCharge(Request $request)
     {
@@ -101,8 +167,8 @@ class StripeController extends Controller
     /**
      * Retrieve all charges
      *
-     * @return \Stripe\Collection
-     * @throws \Stripe\Exception\ApiErrorException
+     * @return Collection
+     * @throws ApiErrorException
      */
     public function retrieveCharges()
     {
@@ -110,11 +176,126 @@ class StripeController extends Controller
         return $charges;
     }
 
+
+    /**
+     * Create a charge from customer
+     *
+     * @return \Stripe\PaymentMethod
+     * @throws ApiErrorException
+     */
+    public function createPaymentMethod()
+    {
+        $paymentMethod = $this->stripe->paymentMethods->create(
+            [
+                'type' => 'card',
+                'card' => [
+                    'number'    => '4242424242424242',
+                    'exp_month' => 12,
+                    'exp_year'  => 2021,
+                    'cvc'       => '314',
+                ],
+            ]
+        );
+        return $paymentMethod;
+    }
+
+    /**
+     * Create a charge from customer
+     *
+     * @return \Stripe\PaymentMethod
+     * @throws ApiErrorException
+     */
+    public function attachPaymentMethod(Request $request)
+    {
+        $paymentMethod = $this->stripe->paymentMethods->attach($request->payment_method_id, ['customer' => $request->customer_id]);
+        return $paymentMethod;
+    }
+
+    /**
+     * Create a charge from customer
+     *
+     * @return \Stripe\PaymentIntent
+     * @throws ApiErrorException
+     */
+    public function createPaymentIntent()
+    {
+        $paymentIntent = $this->stripe->paymentIntents->create(
+            [
+                'amount'               => 2000,
+                'currency'             => 'usd',
+                'payment_method_types' => ['card'],
+                'confirm'              => true,
+            ]
+        );
+        return $paymentIntent;
+    }
+
+    /**
+     * Create a charge from customer
+     *
+     * @return \Stripe\PaymentIntent
+     * @throws ApiErrorException
+     */
+    public function createPaymentIntentHold(Request $request)
+    {
+        $paymentIntent = $this->stripe->paymentIntents->create(
+            [
+                'amount'               => 20000,
+                'currency'             => 'usd',
+                'payment_method_types' => ['card'],
+                'confirm'              => true,
+                'capture_method'       => 'manual',
+                'payment_method'       => $request->payment_method_id,
+                'customer'             => $request->customer_id,
+            ]
+        );
+
+        return $paymentIntent;
+    }
+
+    /**
+     * Create a charge from customer
+     *
+     * @return \Stripe\PaymentIntent
+     * @throws ApiErrorException
+     */
+    public function capturePaymentIntentHold(Request $request)
+    {
+        $paymentIntent = $this->stripe->paymentIntents->capture($request->payment_intent_id, []);
+        return $paymentIntent;
+    }
+
+    /**
+     * Create a charge from customer
+     *
+     * @return \Stripe\PaymentIntent
+     * @throws ApiErrorException
+     */
+    public function cancelPaymentIntentHold(Request $request)
+    {
+        $paymentIntent = $this->stripe->paymentIntents->cancel($request->payment_intent_id, [
+            'cancellation_reason' => 'abandoned'
+        ]);
+        return $paymentIntent;
+    }
+
+    /**
+     * Create a charge from customer
+     *
+     * @return \Stripe\PaymentIntent
+     * @throws ApiErrorException
+     */
+    public function retrievePaymentIntent(Request $request)
+    {
+        $paymentIntent = $this->stripe->paymentIntents->retrieve($request->payment_intent_id, []);
+        return $paymentIntent;
+    }
+
     /**
      * Retrieve merchant balance
      *
      * @return Balance
-     * @throws \Stripe\Exception\ApiErrorException
+     * @throws ApiErrorException
      */
     public function retrieveBalance()
     {
@@ -125,8 +306,8 @@ class StripeController extends Controller
     /**
      * Retrieve transaction history for merchant
      *
-     * @return \Stripe\Collection
-     * @throws \Stripe\Exception\ApiErrorException
+     * @return Collection
+     * @throws ApiErrorException
      */
     public function retrieveTransactionHistory()
     {
--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('customers', 'StripeController@customers');
 Route::get('customer/create', 'StripeController@customerCreate');
-Route::get('customer/create/charge', 'StripeController@createCharge');
-Route::get('customer/retrieve/charge', 'StripeController@retrieveCharge');
-Route::get('customer/retrieve/charges', 'StripeController@retrieveCharges');
+Route::get('customer/retrieve', 'StripeController@retriveCustomer');
+Route::get('customer/update', 'StripeController@updateCustomer');
+Route::delete('customer/delete', 'StripeController@deleteCustomer');
+
 Route::get('customer/authorize/hold', 'StripeController@authorizeHold');
 Route::get('customer/authorize/hold/capture', 'StripeController@authorizeHoldCapture');
 
+Route::get('customer/create/payment-method', 'StripeController@createPaymentMethod');
+Route::get('customer/attach/payment-method', 'StripeController@attachPaymentMethod');
+
+Route::get('customer/create/charge', 'StripeController@createCharge');
+Route::get('customer/retrieve/charge', 'StripeController@retrieveCharge');
+Route::get('customer/retrieve/charges', 'StripeController@retrieveCharges');
+
+Route::get('customer/create/payment-intent', 'StripeController@createPaymentIntent');
+Route::get('customer/create/payment-intent/hold', 'StripeController@createPaymentIntentHold');
+Route::get('customer/capture/payment-intent', 'StripeController@capturePaymentIntentHold');
+Route::get('customer/cancel/payment-intent', 'StripeController@cancelPaymentIntentHold');
+Route::get('customer/retrieve/payment-intent', 'StripeController@retrievePaymentIntent');
+Route::get('customer/retrieve/payment-intents', 'StripeController@retrieveCharges');
 
 Route::get('merchant/retrieve/balance', 'StripeController@retrieveBalance');
 Route::get('merchant/retrieve/transactions', 'StripeController@retrieveTransactionHistory');
